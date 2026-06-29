236ffdc1581bf6a532a6635b993ef4ae8af78b27
monushah108 <sonuk627422@gmail.com>
2026-06-29 17:43:05 +0530

    linear search

--- Array/array.js
+++ Array/array.js
@@ -1,25 +1,37 @@
 // move zeros to end
 
-const nums = [0, 1, 4, 0, 5, 2];
+// const nums = [0, 1, 4, 0, 5, 2];
 
-function moveZeros(nums) {
-  let n = nums.length;
-  let k = 0;
-  for (let i = 0; i < n; i++) {
-    if (nums[i] !== 0) {
-      nums[k] = nums[i];
-      k++;
-    }
-  }
+// function moveZeros(nums) {
+//   let n = nums.length;
+//   let k = 0;
+//   for (let i = 0; i < n; i++) {
+//     if (nums[i] !== 0) {
+//       nums[k] = nums[i];
+//       k++;
+//     }
+//   }
 
-  console.log(k);
+//   console.log(k);
 
-  while (k < n) {
-    nums[k] = 0;
-    k++;
-  }
+//   while (k < n) {
+//     nums[k] = 0;
+//     k++;
+//   }
 
-  return nums;
-}
+//   return nums;
+// }
 
-console.log(moveZeros(nums));
+// linear Search
+// let nums = [2, 3, 4, 5, 3];
+// let target = 6;
+
+// function LinerS(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == target) return 1;
+//   }
+
+//   return -1;
+// }
+
+// console.log(LinerS(nums, target));
